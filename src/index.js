9fd2d19b80f05f5ae35a98a5416610dd7c68cd70
joebochill <boyle.p.joseph@gmail.com>
2019-07-08 14:53:37 -0400

    require typeface

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import './style.css';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import * as PXBThemes from '@pxblue/themes/react';
+require('typeface-open-sans');
 
 render(
   <MuiThemeProvider theme={createMuiTheme(PXBThemes.blue)}>
