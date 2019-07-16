3d57ce780fad2590887fca462d6afa8ff17e68d3
Chinna <cm.reddy071@gmail.com>
2019-07-16 14:38:39 -0400

    Upgraded to Angular8

--- src/app/shared/un-camel-case.pipe.spec.ts
+++ src/app/shared/un-camel-case.pipe.spec.ts
@@ -1,38 +1,8 @@
-
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from '../app.component';
-import {
-  MatAutocompleteModule,
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatChipsModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatExpansionModule,
-  MatGridListModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatNativeDateModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatRippleModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatSliderModule,
-  MatSlideToggleModule,
-  MatSnackBarModule,
-  MatStepperModule,
-  MatTableModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule,
-} from '@angular/material';
+import { MatListModule} from '@angular/material/list';
+import { MatToolbarModule} from '@angular/material/toolbar';
 import {UnCamelCasePipe} from './un-camel-case.pipe';
 
 
@@ -49,4 +19,3 @@ describe('Pipe: Default', () => {
     })
 
 });
-
