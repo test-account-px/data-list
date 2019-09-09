7afc9e38711b793d2566d8a3221e174ec6e99a47
Chinna Gurrala <cm.reddy071@gmail.com>
2019-09-09 13:51:51 -0400

    Created ReactNative local example

--- App.js
+++ App.js
@@ -3,25 +3,34 @@ import { StyleSheet, Text, View } from 'react-native';
 import * as Colors from '@pxblue/colors'
 import { Header, ListItem } from 'react-native-elements';
 const data = [{
-  name: "John",
-  value: 123,
+  name: "George Washington",
+  value: 1789,
 }, {
-  name: "Bob",
-  value: 456,
-}]
+  name: "Jhon Adams",
+  value: 1796,
+},{
+  name: "Thomas Jefferson",
+  value: 1800,
+}, {
+  name: "James Madison",
+  value: 1808,
+}, {
+  name: "James Monore",
+  value: 1812,
+}, ]
 export default function App() {
   return (
     <View style={styles.container}>
       <Header
         backgroundColor={Colors.blue[500]}
-        centerComponent={{ text: 'Empty States', style: { color: '#fff', fontSize: 16, } }}
+        centerComponent={{ text: 'Simple List', style: { color: '#fff', fontSize: 18, } }}
       />
       {
         data.map((item, index) => (
           <ListItem
             key={index}
             title={item.name}
-            rightTitle={item.value}
+            rightTitle={`${item.value}`}
           />
         ))
       }
